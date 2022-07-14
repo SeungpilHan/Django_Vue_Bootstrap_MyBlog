2c99807200daefcfafed291990f0598baf15f15b
angelkim88 <gkstmdvlf2@gmail.com>
2022-07-14 09:03:01 +0900

    Django Scrollspy

--- VueDjAgency/mysite/urls.py
+++ VueDjAgency/mysite/urls.py
@@ -13,14 +13,19 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from mysite.views import HomeView
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # shRim
-    path('', HomeView.as_view(), name='home')
+    # shkim
+    path('', HomeView.as_view(), name='home'),
+    path('blog/', include('blog.urls')),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
